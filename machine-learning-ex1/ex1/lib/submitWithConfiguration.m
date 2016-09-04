6e84d9acf13fb6df3f8cb811176cc04788e19e80
Alex Kopytov <alex.kopytov@gmail.com>
2016-09-05 00:23:50 +0300

    Applied peer certificate patch.
    To solve error "urlread: peer certificate...".

--- machine-learning-ex1/ex1/lib/submitWithConfiguration.m
+++ machine-learning-ex1/ex1/lib/submitWithConfiguration.m
@@ -63,7 +63,9 @@ function response = submitParts(conf, email, token, parts)
   body = makePostBody(conf, email, token, parts);
   submissionUrl = submissionUrl();
   params = {'jsonBody', body};
-  responseBody = urlread(submissionUrl, 'post', params);
+  % responseBody = urlread(submissionUrl, 'post', params);
+  % Peer certificate patch. https://www.coursera.org/learn/machine-learning/discussions/forums/8LDwTL2SEeSEJSIACyEKsQ/threads/_et09TGlEea-YhKdNUv9pQ
+  [code, responseBody] = system(sprintf('echo jsonBody=%s | curl -k -X POST -d @- %s', body, submissionUrl));
   response = loadjson(responseBody);
 end
 
