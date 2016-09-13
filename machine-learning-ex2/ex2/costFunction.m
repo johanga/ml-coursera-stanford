d959f44c9da70d66fdaba7a4ee51dac28ffa86be
Alex Kopytov <alex.kopytov@gmail.com>
2016-09-13 23:23:18 +0300

    Solved assignment for classification problem.

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -20,12 +20,8 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
-
-
-
-
-
-
+J = ( 1 / m ) * sum( -y' * log( sigmoid( X * theta ) ) - ( 1 - y )' * log( 1 - sigmoid( X * theta ) ) );
+grad = (1 / m) * sum( X .* repmat( ( sigmoid( X * theta ) - y ), 1, size( X, 2 ) ) );
 
 % =============================================================
 
--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,10 +17,9 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
-
-
-
-
+J = ( ( 1 / m ) * sum( -y' * log( sigmoid( X*theta ) ) - ( 1 - y )' * log( 1 - sigmoid( X * theta) ) ) ) + ( lambda / ( 2 * m ) ) * sum( theta( 2:length(theta) ) .* theta( 2:length(theta) ) );
+grad = ( 1 / m ) * sum( X .* repmat( ( sigmoid( X * theta ) - y ), 1, size( X, 2 ) ) );
+grad( :, 2:length(grad) ) = grad( :, 2:length(grad) ) + ( lambda / m ) * theta( 2:length(theta) )';
 
 % =============================================================
 
